7e3087f4c4c44337b616b77f1ccc2d4a4725aaac
Gustavo <gustavo.martinez201@tectijuana.edu.mx>
2022-04-27 20:57:18 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const SimplexMethod = require('./simplex')
 
 const options = {
     // the name of a file with a matrix to be solved
-    filename: 'inputs/matrix2.txt',
+    filename: 'inputs/matrix1.txt',
     // [optional] prints the result & steps to a file, not to the console
     toFile: true,
     // [optional] where to save the result; is needed only if the toFile option
